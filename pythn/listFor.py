eea347b2c3e10e28aa179af48cd1e41578c5bce7
Andrew0324x <126356257+Andrew0324x@users.noreply.github.com>
2023-03-19 20:58:50 -0700

    Week 3

---
+++ pythn/listFor.py
@@ -0,0 +1,14 @@
+fruits = ['apple', 'banana', 'melon']
+students = ['yes', 'no']
+
+fruits.append('mango')
+fruits.append('dragon fruit')
+
+print(fruits)
+
+
+students.append('maybe')
+print(students)
+
+for student in students:
+    print(student)
