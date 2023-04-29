223b58a37ed88f9bfcaabd1e0632dab841a8eb42
Andrew0324x <126356257+Andrew0324x@users.noreply.github.com>
2023-04-29 10:11:19 -0700

    Add files via upload

--- Game/Project.py
+++ Game/Project.py
@@ -49,15 +49,41 @@ bullet_speed = 1.2
 # Enemy
 enemy = pygame.image.load(os.path.join(image_path, "P_enemy.png"))
 enemy_size = enemy.get_rect().size
-enemy_width = enemy_size[0]
-enemy_height = enemy_size[1]
-enemy_x_pos = (screen_width) + enemy_width
-enemy_y_pos = (screen_height /2)- (enemy_height /2)
-enemy_speed = 300
 
+enemy_images = [
+    pygame.image.load(os.path.join(image_path, "P_enemy.png")),
+    pygame.image.load(os.path.join(image_path, "P_enemy_2.png")),
+    pygame.image.load(os.path.join(image_path, "P_enemy_3.png"))
+]
+
+enemy_speed_x = 3
+
+enemies = []
+
+enemies.append({
+    "pos_x" : (screen_width) - enemy_size[0],
+    "pos_y" : random.randint(0, screen_height - enemy_size[1]),
+    "img_idx" : 0,
+    "to_x" : enemy_speed_x,
+    "to_y" : 0
+})
+enemies.append({
+    "pos_x" : (screen_width) - enemy_size[0],
+    "pos_y" : random.randint(0, screen_height - enemy_size[1]),
+    "img_idx" : 0,
+    "to_x" : enemy_speed_x,
+    "to_y" : 0
+})
+enemies.append({
+    "pos_x" : (screen_width) - enemy_size[0],
+    "pos_y" : random.randint(0, screen_height - enemy_size[1]),
+    "img_idx" : 0,
+    "to_x" : enemy_speed_x,
+    "to_y" : 0
+})
 # Remove bullets
-bullet_to_remove = 3
-
+bullet_to_remove = -1
+enemy_to_remove = -1
 
 # Config
 game_font = pygame.font.Font(None,40)
@@ -89,56 +115,128 @@ while running:
     # Character position
     character_to_y += 0.01
     character_y_pos += character_to_y * frame
-
+    if character_y_pos < 0 :
+            character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+            character_y_pos = screen_height - character_height
     # Bullets position
     bullets = [[b[0] + bullet_speed, b[1]] for b in bullets]
     bullets = [[b[0], b[1]] for b in bullets if b[0] < 1024]
 
     # Enemies position
-    enemy_x_pos -= enemy_speed
+    for enemy_idx, enemy_val in enumerate(enemies) :
+        enemy_x_pos = enemy_val["pos_x"]
+        enemy_y_pos = enemy_val["pos_y"]
+        enemy_img_idx = enemy_val["img_idx"]
 
-    if enemy_x_pos < 0 :
-        enemy_x_pos = screen_width + enemy_width
+        enemy_size = enemy_images[enemy_img_idx].get_rect().size
+        enemy_width = enemy_size[0]
+        enemy_height = enemy_size[1]
 
-    if character_x_pos < 0 :
-        character_x_pos = 0
-    elif character_x_pos > screen_width - character_width:
-        character_x_pos = screen_width - character_width
+        if enemy_x_pos < 0:
+            enemy_val["pos_x"] = screen_width
 
-    if character_y_pos < 0:
-        character_y_pos = 0
-    elif character_y_pos > screen_height - character_height:
-        character_y_pos = screen_height - character_height
-    
+        enemy_val["pos_x"] -= enemy_val["to_x"]
+
+    # Collision    
     character_rect = character.get_rect()
     character_rect.left = character_x_pos
     character_rect.top = character_y_pos
 
-    enemy_rect = enemy.get_rect()
-    enemy_rect.left = enemy_x_pos
-    enemy_rect.top = enemy_y_pos
+    for enemy_idx, enemy_val in  enumerate(enemies):
+        enemy_x_pos = enemy_val["pos_x"]
+        enemy_y_pos = enemy_val["pos_y"]
+        enemy_img_idx = enemy_val["img_idx"]
 
-    if character_rect.colliderect(enemy_rect):
-        print("Collider !")
-        running = False
+        enemy_rect = enemy_images[enemy_img_idx].get_rect()
+        enemy_rect.left = enemy_x_pos
+        enemy_rect.top = enemy_y_pos
 
+        #enemy vs character
+        if character_rect.colliderect(enemy_rect):
+            running = False
+            game_result = "Game Over"
+            break
+
+        #enemy vs bullets
+        for bullet_idx, bullet_val in enumerate(bullets):
+            bullet_x_pos = bullet_val[0]
+            bullet_y_pos = bullet_val[1]
+
+            #bullet's rect(virture box) update
+            bullet_rect = bullet.get_rect()
+            bullet_rect.left = bullet_x_pos
+            bullet_rect.top = bullet_y_pos
+
+            # Check Collision with enemy
+            if bullet_rect.colliderect(enemy_rect):
+                bullet_to_remove = bullet_idx
+                enemy_to_remove = enemy_idx
+
+                # Change enemy status
+                if enemy_img_idx < 2 : 
+                    enemy_width = enemy_rect.size[0]
+                    enemy_height = enemy_rect.size[1]
+
+                    next_enemy_rect = enemy_images[enemy_img_idx+1].get_rect()
+                    next_enemy_width = next_enemy_rect.size[0]
+                    next_enemy_height = next_enemy_rect.size[1]
+
+                    # Enemy Change
+                    enemies.append({
+                        "pos_x" : enemy_x_pos,
+                        "pos_y" : enemy_y_pos,
+                        "img_idx" : enemy_img_idx + 1,
+                        "to_x" : enemy_speed_x * 1.5,
+                        "to_y" : 0
+                    })
+                break
+        else:
+            continue
+        break
+
+    # Remove collided bullets and enemies
+    if enemy_to_remove > -1 :
+        del enemies[enemy_to_remove]
+        enemy_remove = -1
+
+    if bullet_to_remove > -1:
+        del bullets[bullet_to_remove]
+        bullet_to_remove = -1
+
+    # Print sprites to game window
     screen.blit(background, (0, 0))
     screen.blit(enemy, (enemy_x_pos, enemy_y_pos))
-    screen.blit(character, (character_x_pos, character_y_pos))
-    
-    for bullet_x_pos, bullet_y_pos in bullets:
+
+    for bullet_x_pos, bullet_y_pos in bullets : 
         screen.blit(bullet, (bullet_x_pos, bullet_y_pos))
 
+    for idx, val in enumerate(enemies):
+        enemy_x_pos = val["pos_x"]
+        enemy_y_pos = val ["pos_y"]
+        enemy_img_idx = val["img_idx"]
+        screen.blit(enemy_images[enemy_img_idx], (enemy_x_pos, enemy_y_pos))
+    
+    screen.blit(character, (character_x_pos, character_y_pos))
+
+    # Set timer
     past_time = (pygame.time.get_ticks() - start_ticks) / 1000
     timer = game_font.render(str(round((total_time-past_time),2)), True, (255, 255, 255))
-
     screen.blit(timer, (10,10))
 
+    # Surviving during time end
     if total_time - past_time <= 0:
-        print("time out")
+        game_result = "Mission Complete ! " 
         running = False
 
     pygame.display.update()
 
+# Win / lose message
+msg = game_font.render(game_result, True, (255, 255, 0)) # 255, 255, 0 => Yellow
+msg_rect = msg.get_rect(center=(int(screen_width / 2), int(screen_height / 2)))
+screen.blit(msg, msg_rect)
+pygame.display.update()
+
+# Quit Game
 pygame.time.delay(2000)
 pygame.quit()
